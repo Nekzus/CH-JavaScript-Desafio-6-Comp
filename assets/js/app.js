d01a2cfe2fde1b906419af67df4b2ec4af2463a3
Nekzus <85507589+Nekzus@users.noreply.github.com>
2021-10-18 19:23:35 -0300

    Codigo completo

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,17 +25,9 @@ const filtroPorTitulo = (titulo )=> videoJuegos.filter(videoJuego => videoJuego.
 const filtroPorPlataforma = (plataforma )=> videoJuegos.filter(videoJuego => videoJuego.plataforma === plataforma); // Se verifica la existencia del item dentro del array de almacenamiento.
 const filtroPorGenero = (genero )=> videoJuegos.filter(videoJuego => videoJuego.genero === genero); // Se verifica la existencia del item dentro del array de almacenamiento.
 const filtroPorAnio = (anio )=> videoJuegos.filter(videoJuego => videoJuego.anio === anio); // Se verifica la existencia del item dentro del array de almacenamiento.
-const filtroPorStock = (stock )=> videoJuegos.filter(videoJuego => videoJuego.stock === stock); // Se verifica la existencia del item dentro del array de almacenamiento.
 
-//***FUNCIONES DE BUSQUEDA ****//
+//***FUNCION DE BUSQUEDA POR IDENTIFICADOR ITEM ****//
 const busquedaPorIdtm = (idItm) => videoJuegos.find(videoJuego => videoJuego.idItm === idItm); // Se verifica la existencia del item dentro del array de almacenamiento
-const busquedaPorTitulo = (titulo) => videoJuegos.find(videoJuego => videoJuego.titulo === titulo); // Se verifica la existencia del item dentro del array de almacenamiento.
-const busquedaPorPlataforma = (plataforma )=> videoJuegos.find(videoJuego => videoJuego.plataforma === plataforma); // Se verifica la existencia del item dentro del array de almacenamiento.
-const busquedaPorGenero = (genero )=> videoJuegos.find(videoJuego => videoJuego.genero === genero); // Se verifica la existencia del item dentro del array de almacenamiento.
-const busquedaPorAnio = (anio )=> videoJuegos.find(videoJuego => videoJuego.anio === anio); // Se verifica la existencia del item dentro del array de almacenamiento.
-const busquedaPorStock = (stock )=> videoJuegos.find(videoJuego => videoJuego.stock === stock); // Se verifica la existencia del item dentro del array de almacenamiento.
-
-
 
 //**** FUNCIÓN DE SOLICITUD DE INGRESO DE DATOS ****/
 const ingresarDatos = () => {
@@ -44,7 +36,7 @@ const ingresarDatos = () => {
     } else {
      titulo = titulo.toUpperCase()}; // Con fines prácticos, en caso de seleccionar la cancelacion del prompt, se cargara un 'SIN NOMBRE' en la variable.
 
-    let plataforma = prompt("Ingrese plataforma videojuego. [PC]-[XBOX]-[PS]", "PC"); // Se solicita la plataforma, se carga un valor por defecto para facilitar la revisión del desafio.
+    let plataforma = prompt("Ingrese plataforma videojuego. [PC]-[XBOX]-[PS]...", "PC"); // Se solicita la plataforma, se carga un valor por defecto para facilitar la revisión del desafio.
     if(plataforma === null)  { return {} 
     } else {
      plataforma = plataforma.toUpperCase()}; // Con fines prácticos, en caso de seleccionar la cancelacion del prompt, se cargara un 'SIN PLATAFORMA' en la variable.
@@ -58,36 +50,40 @@ const ingresarDatos = () => {
             return {};  
         }
     }
-    repeticion = repeticionPlataforma.length;
 
-    let genero = prompt("Ingrese genero videojuego. [ACCION]-[AVENTURA]-[FPS]", "ACCION"); // Se solicita genero, se carga un valor por defecto para facilitar la revisión del desafio.
+    let genero = prompt("Ingrese genero videojuego. [ACCION]-[AVENTURA]-[FPS]...", "ACCION"); // Se solicita genero, se carga un valor por defecto para facilitar la revisión del desafio.
     if(genero === null)  { return {} 
     } else {
      genero = genero.toUpperCase()}; // Con fines prácticos, en caso de seleccionar la cancelacion del prompt, se cargara un 'SIN GENERO' en la variable.
 
-    let anio = parseInt(prompt("Ingrese año videojuego.", 1997)); // Se solicita año, se carga un valor por defecto para facilitar la revisión del desafio.
+    let anio = parseInt(prompt("Ingrese año videojuego. [1997]-[2001]-[2019]...", 1997)); // Se solicita año, se carga un valor por defecto para facilitar la revisión del desafio.
     if(isNaN(anio)) { return {} }; // Con fines prácticos, en caso de seleccionar la cancelacion del prompt, se cargara un valor de 0 en la variable.
 
     let stock = parseInt(prompt("Ingrese stock videojuego.", 120)); // Se solicita stock, se carga un valor por defecto para facilitar la revisión del desafio.
     if(isNaN(stock)) { return {} }; // Con fines prácticos, en caso de seleccionar la cancelacion del prompt, se cargara un valor de 0 en la variable.
-        
-    return { titulo, plataforma, genero, anio, stock }; // Retorna mediante un objeto literal los datos ingresados.
+    
+    repeticion = repeticionPlataforma.length + 1;
+    let itemDuplicado
+    repeticion >= 2 ? itemDuplicado = true : itemDuplicado= false;
+
+    return { titulo, plataforma, genero, anio, stock, itemDuplicado }; // Retorna mediante un objeto literal los datos ingresados.
 };
 
 //**** OBJECT CONSTRUCTOR ****/
 class VideoJuego {
-    constructor(idItm, titulo, plataforma, genero, anio, stock) { // Recibe los datos ingresados por prompts.
+    constructor(idItm, titulo, plataforma, genero, anio, stock, itemDuplicado) { // Recibe los datos ingresados por prompts.
         this.idItm = idItm;
         this.titulo = titulo;
         this.plataforma = plataforma;
         this.genero = genero;
         this.anio = anio;
         this.stock = stock;
+        this.duplicado = itemDuplicado;
     }
     printConsole() {
         // Método impresion en consola de las propiedades y datos de los objetos.
         console.log(
-            `%cITEM #${this.idItm} - sub.(${repeticion + 1})`,
+            `%cITEM #${this.idItm} - veces ingresado(${repeticion})`,
             "color: black; font-weight: bold; background:#0f0;"
         ); // Se aplica un poco de estilo al encabezado en consola.
         console.log(
@@ -101,7 +97,7 @@ btnEjec.addEventListener("click", () => { // Llamado ejecucion del script median
 
     //**** RAMA PRINCIPAL DE EJECUCIÓN DEL SCRIPT ****/
     do { // Mediante el do.. while se solicita al menos la creación de un objeto, y se pregunta al usuario si desea continuar con la ejecución.
-        const { titulo, plataforma, genero, anio, stock } = ingresarDatos(); // Se realiza la desestructuración del objeto literal recibido de la funcion ingresarDatos.
+        const { titulo, plataforma, genero, anio, stock, itemDuplicado } = ingresarDatos(); // Se realiza la desestructuración del objeto literal recibido de la funcion ingresarDatos.
         if (titulo === undefined) { // Se controla que el dato del nombre del video juego no este vacio.
             
             break;
@@ -118,7 +114,8 @@ btnEjec.addEventListener("click", () => { // Llamado ejecucion del script median
                 plataforma,
                 genero,
                 anio,
-                stock
+                stock,
+                itemDuplicado
             ); // Nuevo objeto creado.
             videoJuegos.push(itemGame);
             localStorage.setItem('videoJuegos', JSON.stringify(videoJuegos));
@@ -144,7 +141,7 @@ btnDelet.addEventListener("click", () => { // Llamado borrado de items mediante 
             confirmacion2 = false;
         } else if (busquedaPorIdtm(itemBorrar) !== undefined) { // Si se comprueba la existencia del item a borrar, se procede con el borrado. 
             confirmacion2 = confirm(`Desea quitar item #${itemBorrar} del listado?`); // Se confirma si en verdad se desea borrar.
-            videoJuegos = JSON.parse(localStorage.getItem('videoJuegos'))
+            videoJuegos = JSON.parse(localStorage.getItem('videoJuegos'));
             let indexItemBorrar = videoJuegos.findIndex(videoJuego => videoJuego.idItm === itemBorrar); // Obtengo el numero de posicion en el array del item a borrar.
             videoJuegos.splice(indexItemBorrar, 1); // Ejecuto el borrado.
             localStorage.setItem('videoJuegos', JSON.stringify(videoJuegos));
@@ -162,72 +159,129 @@ btnDelet.addEventListener("click", () => { // Llamado borrado de items mediante 
 
 btnOrder.addEventListener("click", () => { // Llamado ordenamiento de items mediante click del boton en el HTML.
     
-    //**** ORDENAR ITEMS SEGÚN SELECCIÓN DEL USUARIO ****/
+    //**** MODO VISUALIZACIÓN: ORDENAR Y FILTRAR ITEMS SEGÚN SELECCIÓN DEL USUARIO ****/
     do{
-        itemSeleccionar = parseInt(prompt("Desea\n[1]-Ordenar por Campo\n[2]-Filtrar por Campo\n[12]-Ordenar|Filtrar")); // Se solicita al usuario el numero de item a borrar.
-            if(isNaN(itemSeleccionar)) {
+        itemSeleccionar = parseInt(prompt("Desea\n[1]-Ordenar por Campo\n[2]-Filtrar por Campo")); // Se solicita al usuario el numero de item a borrar.
+        copiaVideoJuegos = [ ...videoJuegos];    
+        if(isNaN(itemSeleccionar)) {
                 confirmacion3 = false
                 break;
-            };
-            console.log(itemSeleccionar); 
-            switch (itemSeleccionar) {
-                case 1:
-                    console.log("ordenar campo");
-                    itemOrdenar = parseInt(prompt("Ordenar por Campo\n[1]-Item#\n[2]-Titulo\n[3]-Plataforma\n[4]-Genero\n[5]-Año\n[6]-Stock")); // Se solicita al usuario el numero de item a borrar.
-                    switch (itemOrdenar){
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 4:
-                            break;
-                        case 5:
-                            break;
-                        case 6:
-                            break;
-                        default:
+        };
+        switch (itemSeleccionar) {
+            case 1:
+                itemOrdenar = parseInt(prompt("Elegir Campo a Ordenar\n[1]-Item#\n[2]-Titulo\n[3]-Plataforma\n[4]-Genero\n[5]-Año\n[6]-Stock")); // Se solicita al usuario el numero de item a borrar.
+                switch (itemOrdenar){
+                    case 1:
+                        itemOrdenar = parseInt(prompt("Ordenar Campo Item#\n[1]-Ascendente\n[2]-Descendente"));
+                        if(itemOrdenar === 1) {
+                            copiaVideoJuegos.sort((a,b) => a.idItm - b.idItm);
+                            console.table(copiaVideoJuegos);
+                        } else if (itemOrdenar === 2) {
+                            copiaVideoJuegos.sort((a,b) => b.idItm - a.idItm);
+                            console.table(copiaVideoJuegos);
+                        } else { 
+                            alert("Operador Invalido" );
+                        }
+                        break;
+                    case 2:
+                        itemOrdenar = parseInt(prompt("Ordenar Campo Titulo\n[1]-Ascendente\n[2]-Descendente"));
+                        if(itemOrdenar === 1) {
+                            copiaVideoJuegos.sort((a,b) => (a.titulo).localeCompare(b.titulo));
+                            console.table(copiaVideoJuegos);
+                        } else if (itemOrdenar === 2) {
+                            copiaVideoJuegos.sort((a,b) => (b.titulo).localeCompare(a.titulo));
+                            console.table(copiaVideoJuegos);
+                        } else { 
+                            alert("Operador Invalido" );
+                        }
+                        break;
+                    case 3:
+                        itemOrdenar = parseInt(prompt("Ordenar Campo Plataforma\n[1]-Ascendente\n[2]-Descendente"));
+                        if(itemOrdenar === 1) {
+                            copiaVideoJuegos.sort((a,b) => (a.plataforma).localeCompare(b.plataforma));
+                            console.table(copiaVideoJuegos);
+                        } else if (itemOrdenar === 2) {
+                            copiaVideoJuegos.sort((a,b) => (b.plataforma).localeCompare(a.plataforma));
+                            console.table(copiaVideoJuegos);
+                        } else { 
+                            alert("Operador Invalido" );
+                        }
+                        break;
+                    case 4:
+                        itemOrdenar = parseInt(prompt("Ordenar Campo Genero\n[1]-Ascendente\n[2]-Descendente"));
+                        if(itemOrdenar === 1) {
+                            copiaVideoJuegos.sort((a,b) => (a.genero).localeCompare(b.genero));
+                            console.table(copiaVideoJuegos);
+                        } else if (itemOrdenar === 2) {
+                            copiaVideoJuegos.sort((a,b) => (b.genero).localeCompare(a.genero));
+                            console.table(copiaVideoJuegos);
+                        } else { 
+                            alert("Operador Invalido" );
+                        }
+                        break;
+                    case 5:
+                        itemOrdenar = parseInt(prompt("Ordenar Campo Año\n[1]-Ascendente\n[2]-Descendente"));
+                        if(itemOrdenar === 1) {
+                            copiaVideoJuegos.sort((a,b) => a.anio - b.anio);
+                            console.table(copiaVideoJuegos);
+                        } else if (itemOrdenar === 2) {
+                            copiaVideoJuegos.sort((a,b) => b.anio - a.anio);
+                            console.table(copiaVideoJuegos);
+                        } else { 
+                            alert("Operador Invalido" );
+                        }
+                        break;
+                    case 6:
+                        itemOrdenar = parseInt(prompt("Ordenar Campo Stock\n[1]-Ascendente\n[2]-Descendente"));
+                        if(itemOrdenar === 1) {
+                            copiaVideoJuegos.sort((a,b) => a.stock - b.stock);
+                            console.table(copiaVideoJuegos);
+                        } else if (itemOrdenar === 2) {
+                            copiaVideoJuegos.sort((a,b) => b.stock - a.stock);
+                            console.table(copiaVideoJuegos);
+                        } else { 
+                            alert("Operador Invalido" );
+                        }
+                        break;
+                    default:
                             alert("Operador Invalido" );
                             break;    
                     }
                     break;
-                case 2:
-                    console.log("filtrar campo");
-                    itemFiltrar = parseInt(prompt("Filtrar por Campo\n[1]-Item#\n[2]-Titulo\n[3]-Plataforma\n[4]-Genero\n[5]-Año\n[6]-Stock")); // Se solicita al usuario el numero de item a borrar.
-                    switch (itemFiltrar){
-                        case 1:
-                            itemFiltrar = parseInt(prompt("Ingresar Item#"))
-                            console.table(filtroPorIdtm(itemFiltrar));
-                            break;
-                        case 2:
-                            itemFiltrar = prompt("Ingresar Titulo").toUpperCase()
-                            console.table(filtroPorTitulo(itemFiltrar));
-                            break;
-                        case 3:
-                            itemFiltrar = prompt("Ingresar Plataforma [PC]-[XBOX]-[PS]").toUpperCase()
-                            console.table(filtroPorPlataforma(itemFiltrar));
-                            break;
-                        case 4:
-                            break;
-                        case 5:
-                            break;
-                        case 6:
-                            break;
-                        default:
-                            alert("Operador Invalido" );
-                            break;    
-                    }
+            case 2:
+                itemFiltrar = parseInt(prompt("Elegir Campo a Filtrar\n[1]-Item#\n[2]-Titulo\n[3]-Plataforma\n[4]-Genero\n[5]-Año")); // Se solicita al usuario el numero de item a borrar.
+                switch (itemFiltrar){
+                    case 1:
+                        itemFiltrar = parseInt(prompt("Ingresar Item#"))
+                        console.table(filtroPorIdtm(itemFiltrar));
+                        break;
+                    case 2:
+                        itemFiltrar = prompt("Ingresar Titulo").toUpperCase()
+                        console.table(filtroPorTitulo(itemFiltrar));
+                        break;
+                    case 3:
+                        itemFiltrar = prompt("Ingresar Plataforma [PC]-[XBOX]-[PS]...").toUpperCase()
+                        console.table(filtroPorPlataforma(itemFiltrar));
+                        break;
+                    case 4:
+                        itemFiltrar = prompt("Ingresar Genero [ACCION]-[AVENTURA]-[FPS]...").toUpperCase()
+                        console.table(filtroPorGenero(itemFiltrar));
+                        break;
+                    case 5:
+                        itemFiltrar = parseInt(prompt("Ingresar Año [1997]-[2001]-[2019]..."))
+                        console.table(filtroPorAnio(itemFiltrar));
+                        break;
+                    default:
+                        alert("Operador Invalido" );
+                        break;    
+                }
                     break;
-                case 12:
-                    console.log("ordenar - filtrar campo");
-                    break;
-                default:
-                    alert("Operador Invalido" );
-                    break;
-            }
-
-            confirmacion3 = confirm("Desea continuar?");
+            default:
+                alert("Operador Invalido" );
+                break;
+        }
+        
+        confirmacion3 = confirm("Desea continuar?");
         
     } while(confirmacion3); // Espera confirmacion de cancelacion del borrado.
 }); // Cierre alcance ejecución boton de ejecucion en el HTML.
